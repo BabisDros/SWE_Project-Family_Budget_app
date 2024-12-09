bdd7d4c74cd29eb2089ef6d579c213eeb280ecd4
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-12-09 17:12:57 +0200

    feature(r3): add MonthlySurplus class

--- FamilyBudgetApp/app/src/main/java/com/android/familybudgetapp/domain/MonthlySurplus.java
+++ FamilyBudgetApp/app/src/main/java/com/android/familybudgetapp/domain/MonthlySurplus.java
@@ -1,14 +1,15 @@
 package com.android.familybudgetapp.domain;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 
 public class MonthlySurplus
 {
     private LocalDateTime date;
     private int surplus;
 
-    public MonthlySurplus(int surplus)
+    public MonthlySurplus(LocalDateTime date)
     {
-        this.date = LocalDateTime.now();
+        setDate(date);
     }
 
     public LocalDateTime getDate()
@@ -25,13 +26,34 @@ public class MonthlySurplus
     {
         if(datetime==null)
         {
-            throw  new IllegalArgumentException("datetime shouldn't be null");
+            throw new IllegalArgumentException("Datetime shouldn't be null");
         }
         this.date = datetime;
     }
 
-    public void addSurplus(int amount)
+    public void addCashFlowToSurplus(CashFlow cashFlow)
     {
-        this.surplus+=amount;
+        if(cashFlow==null)
+        {
+            throw new IllegalArgumentException("CashFlow shouldn't be null");
+        }
+        else if (!validateCashFlow(cashFlow))
+        {
+            throw new IllegalArgumentException("CashFlow date is less than current MonthlySurplus date");
+        }
+        this.surplus+=cashFlow.getAmount();
+    }
+
+    public boolean validateCashFlow(CashFlow cashFlow)
+    {
+        if(cashFlow instanceof Repeating)
+        {
+            Repeating repeating=(Repeating)cashFlow;
+
+            YearMonth cashFlowYearMonthDateEnd=YearMonth.of(repeating.getDateEnd().getYear(),repeating.getDateEnd().getMonth());
+            YearMonth currentYearMonth= YearMonth.of(date.getYear(),date.getMonth());
+            return currentYearMonth.isBefore(cashFlowYearMonthDateEnd);
+        }
+        else  return true;
     }
 }
