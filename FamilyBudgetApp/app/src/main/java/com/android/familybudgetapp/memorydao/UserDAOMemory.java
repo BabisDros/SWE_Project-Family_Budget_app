43011cb21580d1aacfdecd59a0fb7d1a5d739cb4
BabisDros <150071850+BabisDros@users.noreply.github.com>
2024-12-23 20:18:14 +0200

    feat(r4): add basic file structure

---
+++ FamilyBudgetApp/app/src/main/java/com/android/familybudgetapp/memorydao/UserDAOMemory.java
@@ -0,0 +1,5 @@
+package com.android.familybudgetapp.memorydao;
+
+public interface UserDAOMemory
+{
+}
---
+++ FamilyBudgetApp/app/src/test/java/com/android/familybudgetapp/dao/DAOTest.java
@@ -0,0 +1,5 @@
+package com.android.familybudgetapp.dao;
+import static org.junit.Assert.*;
+public class DAOTest
+{
+}
